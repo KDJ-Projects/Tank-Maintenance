c4a5af424159d5e5ba17e2e4ff7bc5b27b90557c
Kurt De Jonghe <kurt.de.jonghe2@gmail.com>
2021-11-07 10:35:49 +0100

    Initial Commit

---
+++ Tank-Maintenance/Tank_MaintenanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tank_MaintenanceApp.swift
+//  Tank-Maintenance
+//
+//  Created by Kurt De Jonghe on 07/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Tank_MaintenanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
